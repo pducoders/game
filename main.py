c64cfa8b8b4994f582306991ca5330690cad2f84
PDU 24-25 <twinoakshomeschool@gmail.com>
2024-10-21 12:52:22 -0400

    you can place different directions

--- main.py
+++ main.py
@@ -110,9 +110,21 @@ class level():
                 #absuoltlty horrible fix asap
                 i.y=1000
                 print(i.y)
-    def place(self):
+    def placeLeft(self):
         if self.playerOnFloor()==True:
             deepbricks.append(sawblade(self.player.x-1,self.player.y))
+
+    def placeRight(self):
+        if self.playerOnFloor()==True:
+            deepbricks.append(sawblade(self.player.x+1,self.player.y))
+
+    def placeUp(self):
+        if self.playerOnFloor()==True:
+            deepbricks.append(sawblade(self.player.x,self.player.y+1))
+
+    def placeDown(self):
+        if self.playerOnFloor()==True:
+            deepbricks.append(sawblade(self.player.x,self.player.y-1))
 
     #stay on blocks &condense int one for loop
     def playerOnFloor(self):
@@ -190,8 +202,14 @@ def on_key_press(space, _):
         level1.mine()
     if key=="L":
         level1.movecamera("up",1)
+        if key=="U":
+            level1.placeLeft()
     if key=="P":
-        level1.place()
+        level1.placeRight()
+    if key=="O":
+        level1.placeUp()
+    if key=="I":
+        level1.placeDown()
     #& add minining
 #run it nothing below here expect for run
 game_window.on_draw = update
